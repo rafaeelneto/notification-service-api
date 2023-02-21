8608a15a11658347a7591c5bf25955a21f502533
Rafael Neto <5102522+rafaeelneto@users.noreply.github.com>
2023-02-21 13:03:44 -0300

    feat: implements prisma repository code

--- src/infrastructure/database/prisma/mappers/prismaNotificationMapper.ts
+++ src/infrastructure/database/prisma/mappers/prismaNotificationMapper.ts
@@ -1,4 +1,14 @@
+import { Content } from '@application/entities/content';
 import { Notification } from '@application/entities/Notification';
+
+type rawNotificationPrisma = {
+  category: string;
+  content: string;
+  recipientId: string;
+  readAt: Date | null | undefined;
+  createdAt: Date;
+  id: string;
+};
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -10,5 +20,12 @@ export class PrismaNotificationMapper {
       createdAt: notification.createdAt,
       id: notification.id,
     };
+  }
+
+  static fromPrisma(raw: rawNotificationPrisma) {
+    return new Notification({
+      ...raw,
+      content: new Content(raw.content),
+    });
   }
 }
--- src/infrastructure/database/prisma/repositories/prismaNotification.repository.ts
+++ src/infrastructure/database/prisma/repositories/prismaNotification.repository.ts
@@ -13,4 +13,48 @@ export class PrismaNotificationRepository implements NotificationRepository {
       data: PrismaNotificationMapper.toPrisma(notification),
     });
   }
+
+  async save(notification: Notification): Promise<string> {
+    const updatedNotification = await this.prismaService.notification.update({
+      where: {
+        id: notification.id,
+      },
+      data: {
+        ...PrismaNotificationMapper.toPrisma(notification),
+      },
+    });
+
+    return updatedNotification.id;
+  }
+  async findByNotificationId(
+    notificationId: string,
+  ): Promise<Notification | null> {
+    const raw = await this.prismaService.notification.findUnique({
+      where: {
+        id: notificationId,
+      },
+    });
+
+    if (!raw) return null;
+
+    return PrismaNotificationMapper.fromPrisma(raw);
+  }
+  async findByRecipientId(recipientId: string): Promise<Notification[]> {
+    const raw = await this.prismaService.notification.findMany({
+      where: {
+        recipientId,
+      },
+    });
+
+    return raw.map(PrismaNotificationMapper.fromPrisma);
+  }
+  async countByRecipientId(recipientId: string): Promise<number> {
+    const count = await this.prismaService.notification.count({
+      where: {
+        recipientId,
+      },
+    });
+
+    return count;
+  }
 }
